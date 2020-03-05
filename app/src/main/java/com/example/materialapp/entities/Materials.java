4cc872b6a530219dd55060119d2c0148df09cf72
Zwydge <zwedge.message@gmail.com>
2020-03-05 13:52:05 +0100

    Add Login Activity and Materials, Users Classes

---
+++ app/src/main/java/com/example/materialapp/entities/Materials.java
@@ -0,0 +1,28 @@
+package com.example.materialapp.entities;
+
+public class Materials {
+
+    private int id;
+    private String name;
+
+    public Materials(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
